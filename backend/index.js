dce8f7d4ba36e8c279bf73a8e96b91911816dea4
sandi pratama <sandi.pratama@tirta.co.id>
2022-01-28 11:29:42 +0700

    Change first commit

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
 require('dotenv').config()
 const express = require('express');
 const app = express();
+const router = require('./routes');
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.use(express.json())
+
+app.use(router);
 
 app.listen(process.env.PORT);
