85befee15d79d0ed7c8db4bbc3ee3c8ba214dbae
sandi pratama <sandi.pratama@tirta.co.id>
2022-01-28 09:36:54 +0700

    Change update structure

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
-const port = 3000;
 
-app.get('/', (res) => {
+app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(port);
+app.listen(process.env.PORT);
